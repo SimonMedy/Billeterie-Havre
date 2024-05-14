2fe36a44d06fb04d0f24bef78fd1e73634606943
Yohannkp <yoharh56@gmail.com>
2024-05-14 15:25:09 +0200

    commit du projet

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,7 +12,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[UniqueEntity('email')]
-#[ApiResource]
+
 class User
 {
     #[ORM\Id]
